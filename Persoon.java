dbcc10241693ad02ccf6e9105ac4963ddf8eac14
wessel <wessel.henkes@casema.nl>
2017-04-03 11:33:19 +0200

    Add files via upload
    de classe Student, Persoon, Docent zijn af

---
+++ Persoon.java
@@ -0,0 +1,62 @@
+package model.persoon;
+
+public abstract class Persoon {
+
+	private String voornaam;
+	private String tussenvoegsel;
+	private String achternaam;
+	private String wachtwoord;
+	private String gebruikersnaam;
+
+	public Persoon(String voornaam, String tussenvoegsel, String achternaam, String wachtwoord, String gebruikersnaam) {
+		this.voornaam = voornaam;
+		this.tussenvoegsel = tussenvoegsel;
+		this.achternaam = achternaam;
+		this.wachtwoord = wachtwoord;
+		this.gebruikersnaam = gebruikersnaam;
+	}
+
+	public String getVoornaam() {
+		return this.voornaam;
+	}
+
+	private String getAchternaam() {
+		return this.achternaam;
+	}
+
+	protected String getWachtwoord() {
+		return this.wachtwoord;
+	}
+
+	public String getGebruikersnaam() {
+		return this.gebruikersnaam;
+	}
+
+	public String getVolledigeAchternaam() {
+		String lVolledigeAchternaam="";
+		if (this.tussenvoegsel != null && this.tussenvoegsel != "" && this.tussenvoegsel.length() > 0) {
+			lVolledigeAchternaam += this.tussenvoegsel + " ";
+		}
+		lVolledigeAchternaam += this.getAchternaam();
+		return lVolledigeAchternaam;
+	}
+
+	public boolean komtWachtwoordOvereen(String pWachtwoord) {
+		boolean lStatus = false;
+		if (this.getWachtwoord().equals(pWachtwoord)) {
+			lStatus = true;
+		}
+		return lStatus;
+	}
+	
+	public boolean equals(Object obj){
+		boolean gelijkenObjecten = false;
+		if  (obj instanceof Persoon){
+			Persoon anderePersoon = (Persoon) obj;
+			if (voornaam == anderePersoon.getVoornaam() && getVolledigeAchternaam() == anderePersoon.getVolledigeAchternaam() && wachtwoord == anderePersoon.getWachtwoord() && gebruikersnaam == anderePersoon.getGebruikersnaam() ){		
+				gelijkenObjecten = true;
+			}
+		}
+		return gelijkenObjecten;
+	}
+}
---
+++ Student.java
@@ -0,0 +1,55 @@
+//checked
+package model.persoon;
+
+
+
+public class Student extends Persoon {
+	private int studentNummer;
+	private String groepId;
+
+	public Student(
+		String pVoornaam, 
+		String pTussenvoegsel, 
+		String pAchternaam, 
+		String pWachtwoord, 
+		String pGebruikersnaam,
+		int pStudentNummer) {
+		super(
+			pVoornaam, 
+			pTussenvoegsel, 
+			pAchternaam, 
+			pWachtwoord, 
+			pGebruikersnaam);
+		this.setStudentNummer(pStudentNummer);
+		this.setGroepId("");
+	}
+
+
+ public String getGroepId() {
+    return this.groepId;	
+  }
+ 
+  public void setGroepId(String pGroepId) {
+    this.groepId= pGroepId;	
+  }
+ 
+	public int getStudentNummer() {
+		return this.studentNummer;
+	}
+
+	private void setStudentNummer(int pStudentNummer) {
+		this.studentNummer = pStudentNummer;
+	}
+	
+	public boolean equals(Object obj){
+		boolean gelijkenObjecten = false;
+		if  (obj instanceof Student){
+			Student andereStudent = (Student) obj;
+			if (studentNummer == andereStudent.studentNummer &&  groepId== andereStudent.groepId && super.equals(obj)){
+				gelijkenObjecten = true;
+			}
+		}
+		return gelijkenObjecten;
+	}
+	
+}
