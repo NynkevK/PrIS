2302c4ecb42de8c3ef1528063690dbf2a4b881b5
Ezri-Mudde <ezri.mudde@student.hu.nl>
2017-04-03 17:23:46 +0200

    Variabelen toegevoegd, constructor moet alleen nog vulRooster hebben

--- src/model/PrIS.java
+++ src/model/PrIS.java
@@ -50,6 +50,7 @@ public class PrIS {
 		deDocenten = new ArrayList<Docent>();
 		deStudenten = new ArrayList<Student>();
 		deKlassen = new ArrayList<Klas>();
+		hetRooster = new Rooster();
 
 		// Inladen klassen
 		vulKlassen(deKlassen);
