0304d9c664d9199ab4a33bf30705d8df91a6d241
Ezri-Mudde <ezri.mudde@student.hu.nl>
2017-04-03 12:07:22 +0200

    De klasse Les toegevoegd

---
+++ src/model/les/Les.java
@@ -0,0 +1,85 @@
+package model.les;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import model.afmelding.Afmelding;
+import model.klas.Klas;
+import model.persoon.Docent;
+import model.persoon.Persoon;
+
+public class Les {
+	private LocalDate datum;
+	private LocalTime startTijd;
+	private LocalTime eindTijd;
+	private String vak;
+	private Docent deDocent;
+	private String locatie;
+	private Klas deKlas;
+	private ArrayList<Afmelding> deAfmeldingen;
+	
+	public Les(LocalDate iDatum, LocalTime iStartTijd, LocalTime iEindTijd, String iVak, Docent iDeDocent, String iLocatie, Klas iDeKlas){
+		datum = iDatum;
+		startTijd = iStartTijd;
+		eindTijd = iEindTijd;
+		vak = iVak;
+		deDocent = iDeDocent;
+		locatie = iLocatie;
+		deKlas = iDeKlas;
+		deAfmeldingen = new ArrayList<Afmelding>();
+	}
+	
+	public void voegAfmeldingToe(Persoon deAfgemelde, String reden){
+		Afmelding afmelding = new Afmelding(reden, deAfgemelde);
+		
+		if (!(deAfmeldingen.contains(afmelding))){
+			deAfmeldingen.add(afmelding);
+		}
+	}
+	public ArrayList<Afmelding> getAfmeldingen(){
+		return new ArrayList<Afmelding>(Collections.unmodifiableList(deAfmeldingen));
+	}
+	
+	public boolean equals(Object object){
+		boolean gelijkeObjecten = false;
+		
+		if( object instanceof Les){
+			Les andereLes = (Les)object;
+			
+			if(this.datum.equals(andereLes.datum) &&
+					this.startTijd.equals(andereLes.startTijd) &&
+					this.eindTijd.equals(andereLes.eindTijd) &&
+					this.vak.equals(andereLes.vak) &&
+					this.deDocent.equals(andereLes.deDocent) &&
+					this.locatie.equals(andereLes.locatie) &&
+					this.deKlas.equals(andereLes.deKlas) &&
+					this.deAfmeldingen.equals(andereLes.deAfmeldingen)){
+				gelijkeObjecten = true;
+			}
+		}
+		
+		return gelijkeObjecten;
+	}
+	
+	public int compareTo(Object object){
+		if(object instanceof Les){
+			Les andereLes = (Les)object;
+			if(this.datum.isAfter(andereLes.datum)){
+				return 1; //deze les is later
+			} else if(this.datum.isBefore(andereLes.datum)){
+				return -1; //deze les is eerder
+			} else if(this.datum.equals(andereLes.datum)){
+				if(this.startTijd.isAfter(andereLes.startTijd)){
+					return 1; //deze les begint later op dezelfde dag
+				} else if(this.startTijd.isBefore(andereLes.startTijd)){
+					return -1; //deze les begint eerder op dezelfde dag
+				}
+			}
+			
+		}
+		
+		return 0; //de Lessen zijn op de zelfde datum en tijd
+	}
+}
