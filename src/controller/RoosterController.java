9c58f80f554013173d203ff86fbeca92e43b6d31
Ezri-Mudde <ezri.mudde@student.hu.nl>
2017-04-05 17:49:28 +0200

    RoosterController toegevoegd inclusief alle methoden

---
+++ src/controller/RoosterController.java
@@ -0,0 +1,177 @@
+package controller;
+
+import java.util.ArrayList;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+import model.PrIS;
+import model.rooster.Rooster;
+import model.klas.Klas;
+import model.les.Les;
+import model.persoon.Docent;
+import server.Conversation;
+import server.Handler;
+
+public class RoosterController implements Handler {
+	private PrIS informatieSysteem;
+	
+	public RoosterController(PrIS infoSys){
+		informatieSysteem = infoSys;
+	}
+
+	@Override
+	public void handle(Conversation conversation) {
+		if (conversation.getRequestedURI().startsWith("/rooster/ophalen/klas")){
+			ophalenKlas(conversation);
+		} else if (conversation.getRequestedURI().startsWith("/rooster/ophalen/docent")){
+			ophalenDocent(conversation);
+		} else if (conversation.getRequestedURI().startsWith("/rooster/ophalen")){
+			ophalenAlles(conversation);
+		}
+	}
+
+	private void ophalenKlas(Conversation conversation) {
+		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+		Rooster hetRooster = informatieSysteem.getRooster();
+		
+		// info om juiste rooster op te halen verzamelen
+		String gebruikersnaam = lJsonObjectIn.getString("username");
+		Klas opgevraagdeKlas = informatieSysteem.getKlasVanStudent(informatieSysteem.getStudent(gebruikersnaam));
+		
+		// rooster van de klas ophalen en sorteren
+		ArrayList<Les> roosterVanKlas = hetRooster.getLessenVanKlas(opgevraagdeKlas);
+		roosterVanKlas.sort(null);
+		
+		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
+		
+		for(Les lLes : roosterVanKlas){
+			JsonObjectBuilder lJsonObjectBuilderVoorLes = Json.createObjectBuilder();
+
+			// verzamel de info van de les
+			@SuppressWarnings("rawtypes")
+			ArrayList lesInfo = lLes.getInfo();
+			String datumVanLesString = lesInfo.get(0).toString();
+			String startTijdVanLesString = lesInfo.get(1).toString();
+			String eindTijdVanLesString = lesInfo.get(2).toString();
+			String vakVanLes = (String)lesInfo.get(3);
+			Docent docentVanLes = (Docent)lesInfo.get(4);
+			String gebruikersNaamVanDocent = docentVanLes.getGebruikersnaam();
+			String locatieVanLes = (String)lesInfo.get(5);
+			Klas klasVanLes = (Klas)lesInfo.get(6);
+			String klasCodeVanLes = klasVanLes.getKlasCode();
+			
+			// voeg de info toe aan het json object
+			lJsonObjectBuilderVoorLes
+				.add("date", datumVanLesString)
+				.add("start-time", startTijdVanLesString)
+				.add("end-time", eindTijdVanLesString)
+				.add("course", vakVanLes)
+				.add("teacher", gebruikersNaamVanDocent)
+				.add("location", locatieVanLes)
+				.add("class-code", klasCodeVanLes);
+			
+			// voeg het sson object van de les toe aan de json array
+			lJsonArrayBuilder.add(lJsonObjectBuilderVoorLes);
+		}
+		
+		// en dan als alle afmeldingen gecheckt zijn antwoorden met de json array
+		String lJsonOutStr = lJsonArrayBuilder.build().toString();
+		conversation.sendJSONMessage(lJsonOutStr);
+	}
+
+	private void ophalenDocent(Conversation conversation) {
+		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+		Rooster hetRooster = informatieSysteem.getRooster();
+		
+		// info om juiste rooster op te halen verzamelen
+		String gebruikersnaam = lJsonObjectIn.getString("username");
+		Docent docent = informatieSysteem.getDocent(gebruikersnaam);
+		
+		// rooster van de docent ophalen en sorteren
+		ArrayList<Les> roosterVanDocent = hetRooster.getLessenVanDocent(docent);
+		roosterVanDocent.sort(null);
+		
+		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
+		
+		for(Les lLes : roosterVanDocent){
+			JsonObjectBuilder lJsonObjectBuilderVoorLes = Json.createObjectBuilder();
+
+			// verzamel de info van de les
+			@SuppressWarnings("rawtypes")
+			ArrayList lesInfo = lLes.getInfo();
+			String datumVanLesString = lesInfo.get(0).toString();
+			String startTijdVanLesString = lesInfo.get(1).toString();
+			String eindTijdVanLesString = lesInfo.get(2).toString();
+			String vakVanLes = (String)lesInfo.get(3);
+			Docent docentVanLes = (Docent)lesInfo.get(4);
+			String gebruikersNaamVanDocent = docentVanLes.getGebruikersnaam();
+			String locatieVanLes = (String)lesInfo.get(5);
+			Klas klasVanLes = (Klas)lesInfo.get(6);
+			String klasCodeVanLes = klasVanLes.getKlasCode();
+			
+			// voeg de info toe aan het json object
+			lJsonObjectBuilderVoorLes
+				.add("date", datumVanLesString)
+				.add("start-time", startTijdVanLesString)
+				.add("end-time", eindTijdVanLesString)
+				.add("course", vakVanLes)
+				.add("teacher", gebruikersNaamVanDocent)
+				.add("location", locatieVanLes)
+				.add("class-code", klasCodeVanLes);
+			
+			// voeg het sson object van de les toe aan de json array
+			lJsonArrayBuilder.add(lJsonObjectBuilderVoorLes);
+		}
+		
+		// en dan als alle afmeldingen gecheckt zijn antwoorden met de json array
+		String lJsonOutStr = lJsonArrayBuilder.build().toString();
+		conversation.sendJSONMessage(lJsonOutStr);
+	}
+
+	private void ophalenAlles(Conversation conversation) {
+		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
+		Rooster hetRooster = informatieSysteem.getRooster();
+		// alle lessen in het rooster ophalen en sorteren
+		ArrayList<Les> alleLessen = hetRooster.getLessen();
+		alleLessen.sort(null);
+		
+		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
+		
+		for(Les lLes : alleLessen){
+			JsonObjectBuilder lJsonObjectBuilderVoorLes = Json.createObjectBuilder();
+
+			// verzamel de info van de les
+			@SuppressWarnings("rawtypes")
+			ArrayList lesInfo = lLes.getInfo();
+			String datumVanLesString = lesInfo.get(0).toString();
+			String startTijdVanLesString = lesInfo.get(1).toString();
+			String eindTijdVanLesString = lesInfo.get(2).toString();
+			String vakVanLes = (String)lesInfo.get(3);
+			Docent docentVanLes = (Docent)lesInfo.get(4);
+			String gebruikersNaamVanDocent = docentVanLes.getGebruikersnaam();
+			String locatieVanLes = (String)lesInfo.get(5);
+			Klas klasVanLes = (Klas)lesInfo.get(6);
+			String klasCodeVanLes = klasVanLes.getKlasCode();
+			
+			// voeg de info toe aan het json object
+			lJsonObjectBuilderVoorLes
+				.add("date", datumVanLesString)
+				.add("start-time", startTijdVanLesString)
+				.add("end-time", eindTijdVanLesString)
+				.add("course", vakVanLes)
+				.add("teacher", gebruikersNaamVanDocent)
+				.add("location", locatieVanLes)
+				.add("class-code", klasCodeVanLes);
+			
+			// voeg het sson object van de les toe aan de json array
+			lJsonArrayBuilder.add(lJsonObjectBuilderVoorLes);
+		}
+		
+		// en dan als alle afmeldingen gecheckt zijn antwoorden met de json array
+		String lJsonOutStr = lJsonArrayBuilder.build().toString();
+		conversation.sendJSONMessage(lJsonOutStr);
+	}
+}
