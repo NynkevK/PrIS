6c3701bef2872ec5eda669f9cacbbfb4270a05ff
Ezri-Mudde <ezri.mudde@student.hu.nl>
2017-04-05 17:03:14 +0200

    opslaanPersoon toegevoegd en errste ophalen en opslaan weggehaald

--- src/controller/PresentieController.java
+++ src/controller/PresentieController.java
@@ -37,7 +37,9 @@ public class PresentieController implements Handler{
 			ophalenLes(conversation);
 		} else if (conversation.getRequestedURI().startsWith("/les/presentie/opslaan")){
 			opslaanLes(conversation);
-		} else if (conversation.getRequestedURI().startsWith("/persoon/presentie/opslaan"));
+		} else if (conversation.getRequestedURI().startsWith("/persoon/presentie/opslaan")){
+			opslaanPersoon(conversation);
+		}
 	}
 
 	private void opslaanLes(Conversation conversation) {
@@ -65,12 +67,12 @@ public class PresentieController implements Handler{
 				JsonObject lAfmelding_jsonObj = lAfmeldingen_jArray.getJsonObject(i);
 				
 				// info van afmelding ophalen
-				int lStudentNrVanAfmelding = lAfmelding_jsonObj.getInt("student-number");
+				int lStudentNrVanAfmelding = lAfmelding_jsonObj.getInt("number");
 				Student lStudentVanAfmelding = informatieSysteem.getStudent(lStudentNrVanAfmelding);
-				String lRedenVanAfmelding = lAfmelding_jsonObj.getString("reason");
+				String lTypeVanAfmelding = lAfmelding_jsonObj.getString("type");
 				
 				// afmelding toevoegen aan les
-				lLesVan_afmelding.voegAfmeldingToe(lStudentVanAfmelding, lRedenVanAfmelding);
+				lLesVan_afmelding.voegAfmeldingToe(lStudentVanAfmelding, lTypeVanAfmelding);
 			}
 		}
 		
@@ -123,7 +125,7 @@ public class PresentieController implements Handler{
 			lJsonObjectBuilderVoorAfmelding
 				.add("number", nummerVanPersoon)
 				.add("name", naamVanPersson)
-				.add("type", lAfmelding.getReden());
+				.add("type", lAfmelding.getType());
 			
 			// json object aan json array toevoegen
 			lJsonArrayBuilder.add(lJsonObjectBuilderVoorAfmelding);
@@ -134,152 +136,42 @@ public class PresentieController implements Handler{
 		conversation.sendJSONMessage(lJsonOutStr);
 	}
 
-	public void ophalen(Conversation conversation){
+	private void opslaanPersoon(Conversation conversation){
 		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
-		String lRol = lJsonObjectIn.getString("rol");
-		String lGebruikersnaam = lJsonObjectIn.getString("username");
+		Rooster hetRooster = informatieSysteem.getRooster();
+		String gebruikersNaamVanPersoon = lJsonObjectIn.getString("username");
 		
-		Persoon dePersoon = null;
-		ArrayList<Les> roosterVanPersoon = null;
-		
-		// kijk wat voor rol de opvrager heeft, en haal dan het juiste rooster op
-		if(lRol == "student"){
-			Student opvragende = informatieSysteem.getStudent(lGebruikersnaam);
-			dePersoon = opvragende;
-			Klas klasVanStudent = informatieSysteem.getKlasVanStudent(opvragende);
-			roosterVanPersoon = informatieSysteem.getRooster().getLessenVanKlas(klasVanStudent);
-		} else if(lRol == "docent"){
-			Docent opvragende = informatieSysteem.getDocent(lGebruikersnaam);
-			dePersoon = opvragende;
-			roosterVanPersoon = informatieSysteem.getRooster().getLessenVanDocent(opvragende);
+		// eerst proberen met de gebruikersnaam een student op te halen
+		Persoon persoonVanAfmelding = informatieSysteem.getStudent(gebruikersNaamVanPersoon);
+		if(persoonVanAfmelding == null){
+			persoonVanAfmelding = informatieSysteem.getDocent(gebruikersNaamVanPersoon);
 		}
 		
-		JsonArrayBuilder lJsonArrayBuilder = Json.createArrayBuilder();
-		
-		for(Les lLes : roosterVanPersoon){
-			ArrayList<Afmelding> deAfmeldingen = lLes.getAfmeldingen();
-			
-			Afmelding afmeldingVanPersoon = null;
-			
-			// kijk of er een afmelding van de persoon is
-			for(Afmelding lAfmelding : deAfmeldingen){
-				if(lAfmelding.getAfgemelde() == dePersoon){
-					afmeldingVanPersoon = lAfmelding;
-				} else {
-					continue;
-				}
-			}
-			
-			JsonObjectBuilder lJsonObjectBuilderVoorLes = Json.createObjectBuilder();
-			
-			// verzamel de info van de les
-			@SuppressWarnings("rawtypes")
-			ArrayList lesInfo = lLes.getInfo();
-			String datumVanLesString = lesInfo.get(0).toString();
-			String startTijdVanLesString = lesInfo.get(1).toString();
-			String eindTijdVanLesString = lesInfo.get(2).toString();
-			String vakVanLes = (String)lesInfo.get(3);
-			Docent docentVanLes = (Docent)lesInfo.get(4);
-			String gebruikersNaamVanDocent = docentVanLes.getGebruikersnaam();
-			String locatieVanLes = (String)lesInfo.get(5);
-			Klas klasVanLes = (Klas)lesInfo.get(6);
-			String klasCodeVanLes = klasVanLes.getKlasCode();
-			
-			// voeg de info toe aan het json object
-			lJsonObjectBuilderVoorLes
-				.add("date", datumVanLesString)
-				.add("start-time", startTijdVanLesString)
-				.add("end-time", eindTijdVanLesString)
-				.add("course", vakVanLes)
-				.add("teacher", gebruikersNaamVanDocent)
-				.add("location", locatieVanLes)
-				.add("class-code", klasCodeVanLes);
-			
-			// voeg de afmelding toe aan het json object als die er is
-			if(afmeldingVanPersoon != null){
-				JsonObjectBuilder lJsonObjectBuilderVoorAfmelding = Json.createObjectBuilder();
-				lJsonObjectBuilderVoorAfmelding.add("reason", afmeldingVanPersoon.getReden());
-				lJsonObjectBuilderVoorLes.add("non-appearance",lJsonObjectBuilderVoorAfmelding);
-			}
-			
-			// voeg het json object toe aan de json array
-			lJsonArrayBuilder.add(lJsonObjectBuilderVoorLes);
-			}
-		// maak er een string van en stuur de terug
-		String lJsonOutStr = lJsonArrayBuilder.build().toString();
-		conversation.sendJSONMessage(lJsonOutStr);
-	}
-	
-	public void opslaan(Conversation conversation){
-		JsonObject lJsonObjectIn = (JsonObject) conversation.getRequestBodyAsJSON();
-		String lRol = lJsonObjectIn.getString("rol");
-		String lGebruikersnaam = lJsonObjectIn.getString("username");
-		Rooster hetRooster = informatieSysteem.getRooster();
-		
-		// JsonArray van nieuwe afmeldingen uit de request halen
 		JsonArray lAfmeldingen_jArray = lJsonObjectIn.getJsonArray("non-appearance");
 		
-		// als de rol van de persoon die de afmelding(en) verstuurd student is wordt die persoon voor de afmeldingen gebruikt
-		if(lRol == "student"){
-			Student afmeldende = informatieSysteem.getStudent(lGebruikersnaam);
-			if(lAfmeldingen_jArray != null){
-				for(int i=0;i<lAfmeldingen_jArray.size();i++){
-					JsonObject lAfmelding_jsonObj = lAfmeldingen_jArray.getJsonObject(i);
-					
-					// info verzamelen om de les mee op te vragen
-					
-					// calendar object converten naar LocalDate en LocalTime
-					@SuppressWarnings("static-access")
-					Calendar lDatum_afmeldingInCal = PrIS.standaardDatumStringToCal(lAfmelding_jsonObj.getString("date")).getInstance();
-					LocalDateTime dateTime = LocalDateTime.ofInstant(lDatum_afmeldingInCal.toInstant(), ZoneId.systemDefault());
-					LocalDate lDatum_afmeldingInLD = dateTime.toLocalDate();
-					LocalTime lStartTijd_afmelding = dateTime.toLocalTime();
-					
-					// locatie van de les van de afmelding uit de request halen
-					String lLocatie_afmelding = lAfmelding_jsonObj.getString("location");
-					
-					// les ophalen waarvoor afgemeld is
-					Les lLesVan_afmelding = hetRooster.getLes(lDatum_afmeldingInLD, lStartTijd_afmelding, lLocatie_afmelding);
-					
-					// reden van de afmelding uit de request halen
-					String lReden_afmelding = lAfmelding_jsonObj.getString("reason");
-					
-					// afmelding toevoegen aan les
-					lLesVan_afmelding.voegAfmeldingToe(afmeldende, lReden_afmelding);
-				}
-			}
-		}
-		
-		if(lRol == "docent"){
-			if(lAfmeldingen_jArray != null){
-				for(int i=0;i<lAfmeldingen_jArray.size();i++){
-					JsonObject lAfmelding_jsonObj = lAfmeldingen_jArray.getJsonObject(i);
-					// afgemelde als student proberen op te halen
-					Persoon afgemelde = informatieSysteem.getStudent(lAfmelding_jsonObj.getString("username"));
-					// als de afgemelde geen student als docent op halen
-					if(afgemelde == null){
-						afgemelde = informatieSysteem.getDocent(lAfmelding_jsonObj.getString("username"));
-					}
-					// info verzamelen om de les mee op te vragen
-					// calendar object converten naar LocalDate en LocalTime
-					@SuppressWarnings("static-access")
-					Calendar lDatum_afmeldingInCal = PrIS.standaardDatumStringToCal(lAfmelding_jsonObj.getString("date")).getInstance();
-					LocalDateTime dateTime = LocalDateTime.ofInstant(lDatum_afmeldingInCal.toInstant(), ZoneId.systemDefault());
-					LocalDate lDatum_afmeldingInLD = dateTime.toLocalDate();
-					LocalTime lStartTijd_afmelding = dateTime.toLocalTime();
-					
-					// locatie van de les van de afmelding uit de request halen
-					String lLocatie_afmelding = lAfmelding_jsonObj.getString("location");
-					
-					// les ophalen waarvoor afgemeld is
-					Les lLesVan_afmelding = hetRooster.getLes(lDatum_afmeldingInLD, lStartTijd_afmelding, lLocatie_afmelding);
-					
-					// reden van de afmelding uit de request halen
-					String lReden_afmelding = lAfmelding_jsonObj.getString("reason");
-					
-					// afmelding toevoegen aan les
-					lLesVan_afmelding.voegAfmeldingToe(afgemelde, lReden_afmelding);
-				}
+		if(lAfmeldingen_jArray != null){
+			for(int i=0;i<lAfmeldingen_jArray.size();i++){
+				JsonObject lAfmelding_jsonObj = lAfmeldingen_jArray.getJsonObject(i);
+				
+				// info verzamelen om de les mee op te vragen
+				
+				// datum en begin tijd ophalen
+				LocalDate lDatumVanAfmelding = LocalDate.parse(lAfmelding_jsonObj.getString("date"));
+				LocalTime lStartTijdVanAfmelding = LocalTime.parse(lAfmelding_jsonObj.getString("start-time"));
+				
+				// locatie van de les van de afmelding uit de request halen
+				String lLocatie_afmelding = lAfmelding_jsonObj.getString("location");
+				
+				// les ophalen waarvoor afgemeld is
+				Les lLesVan_afmelding = hetRooster.getLes(lDatumVanAfmelding, lStartTijdVanAfmelding, lLocatie_afmelding);
+				
+				// reden van de afmelding uit de request halen
+				String lTypeAfmelding = lAfmelding_jsonObj.getString("type");
+				String lRedenAfmelding = lAfmelding_jsonObj.getString("reason");
+				
+				// afmelding toevoegen aan les
+				lLesVan_afmelding.voegAfmeldingMetRedeToe(lTypeAfmelding, lRedenAfmelding, persoonVanAfmelding);
+				
 			}
 		}
 		
