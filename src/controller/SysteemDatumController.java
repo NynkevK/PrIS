4418cc671d24f547e988edfbd15b3b9df848e101
Ezri-Mudde <ezri.mudde@student.hu.nl>
2017-04-05 17:22:08 +0200

    ophalenLesInfo aangepast

--- src/controller/SysteemDatumController.java
+++ src/controller/SysteemDatumController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 
 import model.PrIS;
+import model.les.Les;
+import model.rooster.Rooster;
 import server.Conversation;
 import server.Handler;
 
@@ -31,17 +35,20 @@ public class SysteemDatumController implements Handler {
 	}
 	
   private void ophalenLesInfo(Conversation conversation) {
+  	Rooster hetRooster = informatieSysteem.getRooster();
+  	ArrayList<Les> deLessen = hetRooster.getLessen();
+  	deLessen.sort(null);
   	//<to do> begin
   	//De volgende statements moeten gewijzigd worden zodat daadwerkelijk de eerste en laatste lesdatum wordt bepaald
-  	Calendar lEersteLesDatum = Calendar.getInstance();
-		Calendar lLaatsteLesDatum = Calendar.getInstance();
+  	LocalDate lEersteLesDatum = (LocalDate) deLessen.get(0).getInfo().get(0);
+		LocalDate lLaatsteLesDatum = (LocalDate) deLessen.get(deLessen.size() -1).getInfo().get(0);
     //<to do> end
 		
 		JsonObjectBuilder lJsonObjectBuilder = Json.createObjectBuilder();
 		//Deze volgorde mag niet worden gewijzigd i.v.m. JS. (Hier mag dus ook geen andere JSON voor komen.)
 		lJsonObjectBuilder 
-			.add("eerste_lesdatum", PrIS.calToStandaardDatumString(lEersteLesDatum))
-			.add("laatste_lesdatum", PrIS.calToStandaardDatumString(lLaatsteLesDatum));
+			.add("eerste_lesdatum", lEersteLesDatum.toString())
+			.add("laatste_lesdatum", lLaatsteLesDatum.toString());
 
 		String lJsonOut = lJsonObjectBuilder.build().toString();
 		
